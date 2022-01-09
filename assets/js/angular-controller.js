3265f88aa8ecbe0588ef2bef8c25cd237aab6699
iamavishkar <imavishkar@gmail.com>
2022-01-09 22:04:56 +0530

    commit

--- assets/js/angular-controller.js
+++ assets/js/angular-controller.js
@@ -35,16 +35,47 @@ app.controller("mainController", function($rootScope, $scope){
 app.controller("tableController", function($rootScope, $scope){
     $rootScope.pageTitle = "Table";
     $scope.goBackUrl = "/";
-    $scope.list = [];
+    $scope.listTable = [];
     $scope.resetForm = function(){
         $scope.name = '';
         $scope.email = '';
         $scope.phone = '';
     }
     $scope.submitForm = function(){
-        $scope.list.push({
-            name: $scope.name, email: $scope.email, phone: $scope.phone
-        });
-        $scope.resetForm();
+        if($scope.name == "" || $scope.email == "" || $scope.phone == ""){
+            $scope.error = "All fields are mandatory";
+        }else{
+            $scope.listTable.push({
+                name: $scope.name, email: $scope.email, phone: $scope.phone
+            });
+            $scope.resetForm();
+        }
+    }
+});
+
+//ifsc api
+app.controller("apiController", function($rootScope, $scope, $http){
+    $rootScope.pageTitle = "IFSC API";
+    $scope.goBackUrl = "/";
+    $scope.listApi = "";
+    var url = "https://ifsc.razorpay.com/";
+    //reset form
+    $scope.resetForm = function(){
+        $scope.ifsc = '';
+    }
+    //submit form
+    $scope.submitForm = function(){
+        if($scope.ifsc == ""){
+            $scope.inputError = "IFSC Code cannot be blank";
+        }else{
+            $scope.listApi = "";
+            $http.get(url+$scope.ifsc)
+            .then(function(response){
+                $scope.listApi = response.data;
+            }, function(error){
+                $scope.error = "Invalid IFSC Code";
+            });
+            $scope.resetForm();
+        }
     }
 });
