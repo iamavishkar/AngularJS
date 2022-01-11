61a80fe678c895e13e5371c5675c4233fbc96939
iamavishkar <imavishkar@gmail.com>
2022-01-11 15:08:19 +0530

    commit

--- assets/js/angular-controller.js
+++ assets/js/angular-controller.js
@@ -2,49 +2,50 @@
 var app = angular.module("app", ["ngRoute"]);
 
 //routing
-app.config(function($routeProvider, $locationProvider){
+app.config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
+    $locationProvider.html5Mode(true);
     $routeProvider
-    .when("/", {
-        templateUrl: "main.html",
-        controller: "mainController"
-    })
-    .when("/table", {
-        templateUrl: "table.html",
-        controller: "tableController"
-    })
-    .when("/api", {
-        templateUrl: "api.html",
-        controller: "apiController"
-    });
+        .when("/", {
+            templateUrl: "/views/main.html",
+            controller: "mainController"
+        })
+        .when("/table", {
+            templateUrl: "/views/table.html",
+            controller: "tableController"
+        })
+        .when("/api", {
+            templateUrl: "/views/api.html",
+            controller: "apiController"
+        });
 });
 
 ////  Controllers starts  ////
 
 //main controller
-app.controller("mainController", function($rootScope, $scope){
+app.controller("mainController", function ($rootScope, $scope) {
     $rootScope.pageTitle = "Home";
     $scope.banner_img = "assets/img/angularjs-banner.jpg";
     $scope.list = [
-        {title: "Dynamic Table with Form", href: "#table"},
-        {title: "Get IFSC details by API", href: "#api"}
+        { title: "Dynamic Table with Form", href: "table" },
+        { title: "Get IFSC details by API", href: "api" }
     ];
 });
 
 //form and table controller
-app.controller("tableController", function($rootScope, $scope){
+app.controller("tableController", function ($rootScope, $scope) {
     $rootScope.pageTitle = "Table";
     $scope.goBackUrl = "/";
     $scope.listTable = [];
-    $scope.resetForm = function(){
+    $scope.resetForm = function () {
         $scope.name = '';
         $scope.email = '';
         $scope.phone = '';
     }
-    $scope.submitForm = function(){
-        if($scope.name == "" || $scope.email == "" || $scope.phone == ""){
+    $scope.submitForm = function () {
+        if ($scope.name == "" || $scope.email == "" || $scope.phone == "") {
             $scope.error = "All fields are mandatory";
-        }else{
+        } else {
             $scope.listTable.push({
                 name: $scope.name, email: $scope.email, phone: $scope.phone
             });
@@ -54,27 +55,27 @@ app.controller("tableController", function($rootScope, $scope){
 });
 
 //ifsc api
-app.controller("apiController", function($rootScope, $scope, $http){
+app.controller("apiController", function ($rootScope, $scope, $http) {
     $rootScope.pageTitle = "IFSC API";
     $scope.goBackUrl = "/";
     $scope.listApi = "";
     var url = "https://ifsc.razorpay.com/";
     //reset form
-    $scope.resetForm = function(){
+    $scope.resetForm = function () {
         $scope.ifsc = '';
     }
     //submit form
-    $scope.submitForm = function(){
-        if($scope.ifsc == ""){
+    $scope.submitForm = function () {
+        if ($scope.ifsc == "") {
             $scope.inputError = "IFSC Code cannot be blank";
-        }else{
+        } else {
             $scope.listApi = "";
-            $http.get(url+$scope.ifsc)
-            .then(function(response){
-                $scope.listApi = response.data;
-            }, function(error){
-                $scope.error = "Invalid IFSC Code";
-            });
+            $http.get(url + $scope.ifsc)
+                .then(function (response) {
+                    $scope.listApi = response.data;
+                }, function (error) {
+                    $scope.error = "Invalid IFSC Code";
+                });
             $scope.resetForm();
         }
     }
